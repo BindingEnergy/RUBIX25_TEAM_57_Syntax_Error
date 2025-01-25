e95902b3445ef35dc579bc7aa5e8f5075fbf0279
BindingEnergy <santusht.matra11@hotmail.com>
2025-01-25 15:29:18 +0530

    deleted appwrite files

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URL, {})
     .then(() => {
         console.log('Mongo DB Connected !');
     })
